58efc603326cddb5e8e99f724c5f6da72f374638
Cecilia Gebhard <cirakarma@gmail.com>
2023-01-24 18:19:54 -0300

    Update animatingascii.py

--- animatingascii.py
+++ animatingascii.py
@@ -1,1 +1,15 @@
+import os
+import time
 
+os.system('clear')
+filenames = ["messi02.txt", "messi03.txt", "messi04.txt"]
+frames = []
+
+for name in filenames:
+    with open(name, "r", encoding="utf8") as f:
+        frames.append(f.readlines())
+        
+for frame in frames:
+    print("".join(frame))
+    time.sleep(1)
+    os.system('clear')
